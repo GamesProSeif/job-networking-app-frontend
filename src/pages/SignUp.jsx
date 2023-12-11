f1b23b405a65d7d77d38d41932672a789c7b8dc1
Salah Elshafey <112121144+SalahElshafey@users.noreply.github.com>
2023-12-11 22:51:07 +0200

    Add files via upload

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,74 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            selectedFile: null,
+            errors: {},
+        };
+    }
+
+    handleChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+    };
+
+    handleFileChange = (event) => {
+        this.setState({ selectedFile: event.target.files[0] });
+    };
+
+    validateForm = () => {
+        const { firstName, lastName, email, password, selectedFile } = this.state;
+        const errors = {};
+
+        if (!firstName.trim()) {
+            errors.firstName = 'First name is required';
+        }
+
+        if (!lastName.trim()) {
+            errors.lastName = 'Last name is required';
+        }
+
+        if (!email.trim()) {
+            errors.email = 'Email is required';
+        } else if (!/^\S+@\S+\.\S+$/.test(email)) {
+            errors.email = 'Invalid email address';
+        }
+
+        if (!password.trim()) {
+            errors.password = 'Password is required';
+        }
+
+        if (!selectedFile) {
+            errors.selectedFile = 'CV upload is required';
+        }
+
+        this.setState({ errors });
+
+        return Object.keys(errors).length === 0; // Form is valid if there are no errors
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (this.validateForm()) {
+            // Your form submission logic here
+            console.log('Form submitted successfully:', this.state);
+        } else {
+            console.log('Form validation failed');
+        }
+    };
+
     render() {
+        const { errors } = this.state;
+
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <h3>Sign Up</h3>
 
                 <div className="mb-3">
@@ -12,41 +77,68 @@ export default class SignUp extends Component {
                         type="text"
                         className="form-control"
                         placeholder="First name"
+                        name="firstName"
+                        onChange={this.handleChange}
                     />
+                    {errors.firstName && <div className="text-danger">{errors.firstName}</div>}
                 </div>
+
                 <div className="mb-3">
                     <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Last name"
+                        name="lastName"
+                        onChange={this.handleChange}
+                    />
+                    {errors.lastName && <div className="text-danger">{errors.lastName}</div>}
                 </div>
+
                 <div className="mb-3">
                     <label>Email address</label>
                     <input
                         type="email"
                         className="form-control"
                         placeholder="Enter email"
+                        name="email"
+                        onChange={this.handleChange}
                     />
+                    {errors.email && <div className="text-danger">{errors.email}</div>}
                 </div>
+
                 <div className="mb-3">
                     <label>Password</label>
                     <input
                         type="password"
                         className="form-control"
                         placeholder="Enter password"
+                        name="password"
+                        onChange={this.handleChange}
                     />
+                    {errors.password && <div className="text-danger">{errors.password}</div>}
                 </div>
+
+                <div className="mb-3">
+                    <label>Upload your CV</label>
+                    <input
+                        type="file"
+                        accept=".pdf"
+                        onChange={this.handleFileChange}
+                    />
+                    {errors.selectedFile && <div className="text-danger">{errors.selectedFile}</div>}
+                </div>
+
                 <div className="d-grid">
                     <button type="submit" className="btn btn-primary">
                         Sign Up
                     </button>
                 </div>
+
                 <p className="forgot-password text-right">
                     Already registered <a href="/signin">sign in?</a>
                 </p>
-				<div>
-					<p><b>Upload your CV</b></p>
-				<input type="file" accept=".pdf" onChange={(event) => this.setState({ selectedFile: event.target.files[0] })} />
-				</div>
             </form>
-        )
+        );
     }
 }
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,9 +1,64 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react";
+
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: "",
+            rememberMe: false,
+            errors: {},
+        };
+    }
+
+    handleChange = (event) => {
+        const { name, value, type, checked } = event.target;
+        const val = type === "checkbox" ? checked : value;
+
+        this.setState({
+            [name]: val,
+            errors: {
+                ...this.state.errors,
+                [name]: "", // Clear the previous error when the field is updated
+            },
+        });
+    };
+
+    validateForm = () => {
+        const { email, password } = this.state;
+        const errors = {};
+
+        if (!email.trim()) {
+            errors.email = "Email is required";
+        } else if (!/^\S+@\S+\.\S+$/.test(email)) {
+            errors.email = "Invalid email address";
+        }
+
+        if (!password.trim()) {
+            errors.password = "Password is required";
+        }
+
+        this.setState({ errors });
+        return Object.keys(errors).length === 0;
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (this.validateForm()) {
+            // Your form submission logic here
+            console.log("Form submitted successfully:", this.state);
+        } else {
+            console.log("Form validation failed");
+        }
+    };
+
     render() {
+        const { errors } = this.state;
+
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <h3>Sign In</h3>
 
                 <div className="mb-3">
@@ -12,34 +67,49 @@ export default class Login extends Component {
                         type="email"
                         className="form-control"
                         placeholder="Enter email"
+                        name="email"
+                        onChange={this.handleChange}
                     />
+                    {errors.email && <div className="text-danger">{errors.email}</div>}
                 </div>
+
                 <div className="mb-3">
                     <label>Password</label>
                     <input
                         type="password"
                         className="form-control"
                         placeholder="Enter password"
+                        name="password"
+                        onChange={this.handleChange}
                     />
+                    {errors.password && <div className="text-danger">{errors.password}</div>}
                 </div>
+
                 <div className="mb-3">
                     <div className="custom-control custom-checkbox">
                         <input
                             type="checkbox"
                             className="custom-control-input"
                             id="customCheck1"
+                            name="rememberMe"
+                            onChange={this.handleChange}
                         />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                        <label className="custom-control-label" htmlFor="customCheck1">
+                            Remember me
+                        </label>
                     </div>
                 </div>
+
                 <div className="d-grid">
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <button type="submit" className="btn btn-primary">
+                        Submit
+                    </button>
                 </div>
+
                 <p className="forgot-password text-right">
-                     <a href="#">Forgot password?</a>
+                    <a href="#">Forgot password?</a>
                 </p>
             </form>
         );
     }
 }
-
